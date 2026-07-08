9bf2a7b11f891d4101dfbeab75a313fed8edd345
YASHHTANWARRR <yashtanwar11092005@gmail.com>
2026-07-08 23:39:13 +0530

    Refactor code structure for improved readability and maintainability

--- src/mobile_robot/launch/slam.launch.py
+++ src/mobile_robot/launch/slam.launch.py
@@ -3,37 +3,40 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
 
     slam_params = os.path.join(
-        get_package_share_directory('mobile_robot'),
-        'parameters',
-        'mapper_params_online_async.yaml'
+        get_package_share_directory("mobile_robot"),
+        "parameters",
+        "slam_toolbox.yaml"
     )
 
     scan_frame_corrector = Node(
-        package='mobile_robot',
-        executable='scan_frame_corrector',
-        output='screen'
+        package="mobile_robot",
+        executable="scan_frame_corrector",
+        output="screen"
     )
 
-    slam_toolbox = Node(
-        package='slam_toolbox',
-        executable='async_slam_toolbox_node',
-        name='slam_toolbox',
-        output='screen',
-        parameters=[
-            slam_params,
-            {'use_sim_time': True}
-        ]
+    slam = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("slam_toolbox"),
+                "launch",
+                "online_async_launch.py"
+            )
+        ),
+        launch_arguments={
+            "slam_params_file": slam_params,
+            "use_sim_time": "true",
+        }.items(),
     )
 
-    ld = LaunchDescription()
-
-    ld.add_action(scan_frame_corrector)
-    ld.add_action(slam_toolbox)
-
-    return ld
+    return LaunchDescription([
+        scan_frame_corrector,
+        slam,
+    ])
